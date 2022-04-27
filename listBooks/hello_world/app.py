49d609c6f8186d0cb5d97033331bef5840089b94
JoseProanoSipan <jose.proano@bbva.com>
2022-04-27 16:46:27 -0500

    feat: Getting the query parameters

--- listBooks/hello_world/app.py
+++ listBooks/hello_world/app.py
@@ -2,9 +2,18 @@ from testDatabase import TestDatabase
 
 def lambda_handler(event, context):
 
+
+    ##Getting the query params from the event
+    qpName = event['queryStringParameters']['name']
+    qpAutor = event['queryStringParameters']['autor']
+    qpOffset = event['queryStringParameters']['offset']
+    qpLimit = event['queryStringParameters']['limit']
+
+    args = [qpName,qpAutor,qpOffset,qpLimit]
+
     test = TestDatabase()
 
     return {
         "statusCode": 200,
-        "body": test.listBooks(),
+        "body": test.listBooks(args),
     }
