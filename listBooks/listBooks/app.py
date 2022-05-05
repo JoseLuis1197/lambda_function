47a56285b35d3ff6ef295fc78fbec144f4c6884a
Jose Proano Sipan <joseluisproanosipan@gmail.com>
2022-05-04 22:31:48 -0500

    feat

--- listBooks/listBooks/app.py
+++ listBooks/listBooks/app.py
@@ -3,18 +3,18 @@ import json
 
 def lambda_handler(event, context):
 
-
     ##Getting the query params from the event
-    qpName = event['queryStringParameters']['Name']
-    qpAutor = event['queryStringParameters']['AutorName']
+    qpName = event['queryStringParameters']['name']
+    qpAutor = event['queryStringParameters']['autorName']
 
-    args = [qpName,qpAutor]
+    ##args = [qpName,qpAutor]
 
-    test = TestDatabase()
+    ##test = TestDatabase()
 
-    statusCode, body = test.listBooks(args)
+    ##statusCode, body = test.listBooks(args)
 
     return {
-        "statusCode": statusCode,
+        ##"statusCode": statusCode,
+        "statusCode": 200,
         "body": json.dumps({"data":qpName})
     }
--- listBooks/tests/unit/test_handler.py
+++ listBooks/tests/unit/test_handler.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from hello_world import app
+from listBooks import app
 
 
 @pytest.fixture()
